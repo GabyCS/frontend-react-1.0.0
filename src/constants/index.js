4d22e396eeb07829cf77909729977fbddf4ca7d2
Gabriela Casillas <csgaby7@gmail.com>
2018-10-01 01:01:25 -0500

    configuracion para produccion

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 
-const DEV=true; //  if (DEV == FALSE ) se trabaja en producion
+const DEV=false; //  if (DEV == FALSE ) se trabaja en producion
 
 export const API_URL = (DEV) ? 'http://127.0.0.1:8000/' : 'https://api-tareas.herokuapp.com/';
 export const APP_URL = (DEV) ? 'http://localhost:3000/' : 'https://app-tareas-front.herokuapp.com/';
